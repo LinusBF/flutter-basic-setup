fd33c9155f6923194ffddeb70fd54cf941ded525
LinusBF <contact@linusbf.com>
2021-10-10 17:18:09 +0200

    Added text font and cleaned up main.dart

---
+++ lib/styles/colors.dart
@@ -0,0 +1,6 @@
+import 'dart:ui';
+
+abstract class AppColor {
+  static const Color black = Color(0xFF000000);
+  static const Color blue = Color(0xFF03A9F4);
+}
---
+++ lib/styles/text.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_basic/styles/colors.dart';
+
+abstract class AppText {
+  static const TextStyle h1 =
+      TextStyle(fontFamily: 'NotoSans', color: AppColor.black);
+}
